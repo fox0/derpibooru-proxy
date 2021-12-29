9a951cfe0675520a1af7fc8178732a8391bcc790
fox0 <red-fox0@yandex.ru>
2021-12-29 19:53:06 +0700

    +tags

--- src/templates/get.js
+++ src/templates/get.js
@@ -1,12 +1,31 @@
-(function () {
+function _parse() {
+  var result = '';
   var image_id = window.location.href.match(/\/images\/(\d+)/)[1];
   var data = localStorage.getItem(image_id);
-  //todo if is null
+  if (!data) {
+    return '#' + image_id + ' not found in localStorage';
+  }
   var o = JSON.parse(data);
-  document.getElementById('image').src = o.representations.large;
-  // todo
+  //console.log(o);
+
+  result += '<div class="header">'
+    + '<span>' + o.faves + ' / [' + o.upvotes + ' ' + o.score + ' ' + o.downvotes + ']</span>'
+    + '<a href="' + o.representations.full + '">[full]</a>'
+    + '</div>';
+
+  if (o.mime_type === 'video/webm') {
+    result += '<video src="' + o.representations.large + '" autoplay muted controls></video>';
+  } else {
+    result += '<img src="' + o.representations.large + '"/>';
+  }
+
+  result += '<div class="tags">';
   var tags = o.tags.split(', ');
   for (var i = 0; i < tags.length; i++) {
-    console.log(tags[i]);
+    result += '<a href="/search?q=' + tags[i] + '">' + tags[i] + '</a>';
   }
-})();
+  result += '</div>';
+
+  return result;
+}
+document.getElementById('image').innerHTML = _parse();
